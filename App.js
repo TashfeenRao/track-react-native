0d272061aef773a3826a5f01b9dd5fa9b85e6c36
tashfeen rao <tashfeendev@gmail.com>
2021-06-11 20:27:17 +0500

    started from where i left

--- App.js
+++ App.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-=======
 import React from 'react'
 import { createAppContainer, createSwitchNavigator } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
@@ -55,4 +32,3 @@ export default () => (
         <App />
     </AuthProvider>
 )
->>>>>>> master
--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://63cfd526ae79.ngrok.io',
+    baseURL: 'http://4ae8d2e80613.ngrok.io',
 })
